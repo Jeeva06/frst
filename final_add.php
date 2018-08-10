af046fc351bf1ac095feee5c7c2823aa64ff5a90
JEEVA R <39701973+Jeeva06@users.noreply.github.com>
2018-08-10 09:39:18 -0700

    Update final_add.php

--- final_add.php
+++ final_add.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<hmtl>
+<html>
     <style>
         header{
             font-family:papyrus;
@@ -108,4 +108,4 @@
             <br><br><a href="home.php">HOME</a>
         </div>
     </body>
-</hmtl>
+</html>
